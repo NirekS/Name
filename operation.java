b6b7e80ef5573bb9757b63f171a3a84856085912
NirekS <78590699+NirekS@users.noreply.github.com>
2021-02-05 15:46:46 +0530

    Create operation.java
    This file prints sum and product of 2 input numbers.

---
+++ operation.java
@@ -0,0 +1,7 @@
+public Class operation{
+public static void main(int a,b) {
+int sum=a+b;
+int product=a*b ;
+System.out.prinln("Sum of 2 numbers is"+ sum);
+System.out.prinln("product of 2 numbers is"+ product);
+}}
