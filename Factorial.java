703978fc0ad58fb132f6c161b0bd5a6c2c4ddb53
NirekS <78590699+NirekS@users.noreply.github.com>
2021-02-05 15:59:01 +0530

    Create Factorial.java
    Get the factorial for a input number.

---
+++ Factorial.java
@@ -0,0 +1,10 @@
+public Class Factorial{
+public static void main(int n) {
+int x=1;
+int fact=1
+if(n==1){
+System.out.prinln("Factorial is is"+ fact);}
+while((n>=x)&&(x>0)){
+fact=fact*x; x=x+1;}
+System.out.prinln("Factorial is is"+ fact);
+}}
